d5e0f776d466656cac6b84d51fe3f67b57db68e9
Babajide Tomoshegbo <babajide234@gmail.com>
2022-03-19 13:03:54 +0100

    added git actions for deployment

--- app/Services/Utilities/GetRates.php
+++ app/Services/Utilities/GetRates.php
@@ -61,30 +61,50 @@ class GetRates implements BaseServiceInterface{
     }
     public function getDhlDomestic()
     {
-        // Get origin country zone params and zone
-        $origin = third_country::where('ISO', $this->params['origin'])->first()->Zone;
+        if($this->params['origin'] !== "NG"){
+            // Get origin country zone params and zone
+            $origin = third_country::where('ISO', $this->params['origin'])->first()->Zone;            
+        }else{
+            $origin = third_country::where('ISO', $this->params['origin'])->first()->Zone;
+        }
         $destination = third_country::where('ISO', $this->params['destination'])->first()->Zone;
         $destinationZone = dhl_domestic_zone_matrix::where('zone', $origin )->first()->$destination;
         $table = 'Zone_'.$destinationZone;
         // Get zone price
         $zonePrice = dhl_domestic_zone::where('kg', $this->params['package']['weight'])->first()->$table;
-        return $zonePrice;
+        return (float)$zonePrice;
     }
 
     public function getFedEx(){
 
         $baseUrl = 'https://api.clicknship.com.ng';
         $url = '/clicknship/Operations/DeliveryFee';
+        
+        $origin='';
+        $destination='';
 
+        if(strtoupper($this->params['to']) === 'LAGOS' ){
+            $destination = 'LAGOS MAINLAND';
+        }else{
+            $destination = strtoupper($this->params['to']);
+        }
+
+        if(strtoupper($this->params['from']) === 'LAGOS'){
+            $origin = 'LAGOS MAINLAND';
+        }else{
+            $origin = strtoupper($this->params['from']);
+        }
         $req = [
-            "Origin" => "IBADAN",
-            "Destination" => "ABUJA",
-            "Weight" => "1.5",
+            "Origin" => $origin ,
+            "Destination" => $destination ,
+            "Weight" => $this->params['package']['weight'],
             "PickupType" => "1"
         ];
+        // return $req;
         $response = new HttpRequest($url, $baseUrl,$req);
         
         $response = $response->run();
+        return json_decode($response->getBody());
 
         if(!$response){
             return response()->json(['status' => false, 'message' => 'Server Error'], 500);
@@ -103,6 +123,12 @@ class GetRates implements BaseServiceInterface{
     }
 
     public function getGigm(){
+        // $origin='';
+        // $destination='';
+
+        // if(strtoupper($this->params['from']) ) {
+        //     $origin = 'LAGOS ';
+        // }
         $from = $this->params['from'];
         $to = $this->params['to'];
         $kg = $this->params['package']['weight'];
@@ -111,6 +137,7 @@ class GetRates implements BaseServiceInterface{
         $zone = shiping_zone::where('STATION_NAME',$from)->value($to);
         //get zone price
         $table = 'zone_'.$zone;
+        // return $table;
         $zoneprice = shiping_rate_zone::where('weight', $kg)->value($table);
         //return price
         return $zoneprice;
